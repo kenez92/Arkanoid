bad9f83f238e0559ce46ec2d121869725b737e17
Daniel <daniel.kaminski@smartive.app>
2021-09-14 10:23:05 +0200

    Correct packaging to war file

--- src/main/java/com/kenez92/client/settings/ImageSettings.java
+++ src/main/java/com/kenez92/client/settings/ImageSettings.java
@@ -8,13 +8,13 @@ public class ImageSettings {
     public static final double PLAY_X_POSITION = (BOARD_WIDTH - PLAY_RADIUS) / 2;
     public static final double PLAY_Y_POSITION = (BOARD_HEIGHT - PLAY_RADIUS) / 2;
 
-    public static final String IMAGE_BACKGROUND_PATH = "img/background.jpg";
-    public static final String IMAGE_BALL_PATH = "img/ball.png";
-    public static final String IMAGE_RACKET_PATH = "img/racket.png";
-    public static final String IMAGE_BRICK_YELLOW_PATH = "img/brick_yellow.png";
-    public static final String IMAGE_BRICK_BLUE_PATH = "img/brick_blue.png";
-    public static final String IMAGE_BRICK_RED_PATH = "img/brick_red.png";
-    public static final String IMAGE_GAME_OVER_PATH = "img/game_over.png";
-    public static final String IMAGE_GAME_WIN_PATH = "img/game_win.png";
-    public static final String IMAGE_PLAY_PATH = "img/play.png";
+    public static final String IMAGE_BACKGROUND_PATH = "ArkanoidApplication/img/background.jpg";
+    public static final String IMAGE_BALL_PATH = "ArkanoidApplication/img/ball.png";
+    public static final String IMAGE_RACKET_PATH = "ArkanoidApplication/img/racket.png";
+    public static final String IMAGE_BRICK_YELLOW_PATH = "ArkanoidApplication/img/brick_yellow.png";
+    public static final String IMAGE_BRICK_BLUE_PATH = "ArkanoidApplication/img/brick_blue.png";
+    public static final String IMAGE_BRICK_RED_PATH = "ArkanoidApplication/img/brick_red.png";
+    public static final String IMAGE_GAME_OVER_PATH = "ArkanoidApplication/img/game_over.png";
+    public static final String IMAGE_GAME_WIN_PATH = "ArkanoidApplication/img/game_win.png";
+    public static final String IMAGE_PLAY_PATH = "ArkanoidApplication/img/play.png";
 }
--- src/main/java/com/kenez92/client/settings/SoundSettings.java
+++ src/main/java/com/kenez92/client/settings/SoundSettings.java
@@ -1,10 +1,10 @@
 package com.kenez92.client.settings;
 
 public class SoundSettings {
-    public static final String SOUND_COLLISION_BORDER_PATH = "sounds/collision_border.mp3";
-    public static final String SOUND_COLLISION_RACKET_PATH = "sounds/collision_racket.mp3";
-    public static final String SOUND_COLLISION_BRICK_PATH = "sounds/collision_brick.mp3";
-    public static final String SOUND_GAME_OVER_PATH = "sounds/game_over.mp3";
-    public static final String SOUND_GAME_WIN_PATH = "sounds/game_win.mp3";
-    public static final String SOUND_LIFE_LOST_PATH = "sounds/life_lost.mp3";
+    public static final String SOUND_COLLISION_BORDER_PATH = "ArkanoidApplication/sounds/collision_border.mp3";
+    public static final String SOUND_COLLISION_RACKET_PATH = "ArkanoidApplication/sounds/collision_racket.mp3";
+    public static final String SOUND_COLLISION_BRICK_PATH = "ArkanoidApplication/sounds/collision_brick.mp3";
+    public static final String SOUND_GAME_OVER_PATH = "ArkanoidApplication/sounds/game_over.mp3";
+    public static final String SOUND_GAME_WIN_PATH = "ArkanoidApplication/sounds/game_win.mp3";
+    public static final String SOUND_LIFE_LOST_PATH = "ArkanoidApplication/sounds/life_lost.mp3";
 }
